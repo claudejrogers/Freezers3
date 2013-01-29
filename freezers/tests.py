b9c09312d0436c4c92528a086e8849d6659f51e4
Claude Rogers <claude.j.rogers@gmail.com>
2013-01-28 19:13:28 -0800

    Started writting tests

--- freezers/tests.py
+++ freezers/tests.py
@@ -6,11 +6,26 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from freezers.models import *
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class UtilitiesTest(TestCase):
+    """
+    Test the utilities module. Simple.
+    """
+
+    def test_address_conversion(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that an address is converted to the correct position
         """
-        self.assertEqual(1 + 1, 2)
+        addr1 = 0x0102030405
+        pos1 = getposition(addr1)
+        self.assertEqual(pos1, (1, 2, 3, 4, 5))
+
+    def test_position_conversion(self):
+        """
+        Tests that a position becomes the correct address
+        """
+        pos1 = (3, 4, 1, 2, 81)
+        addr1 = getaddress(pos1)
+        self.assertEqual(addr1, 0x0304010251)
